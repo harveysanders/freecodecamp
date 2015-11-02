d19b41a6b201fdda4577983ca182434ed5e8d5f9
Harvey Sanders <sanders.harvey@gmail.com>
2015-11-01 23:09:35 -0600

    add intial work on tic tac toe game

---
+++ tic_tac_toe/js/script.js
@@ -0,0 +1,27 @@
+(function(){
+	//https://www.quora.com/Is-there-a-way-to-never-lose-at-Tic-Tac-Toe
+	var playerLetter = '';
+	var availableSpaces = [1,2,3,4,5,6,7,8,9];
+
+	$('.player-choice').on('click', function(e){
+		playerLetter = e.target.id;
+		$('.choose-player').hide();
+		$('.game-board').show();
+	});
+
+	$('.square').on('click', function(e){
+		var spaceNum = target.className.charAt(0);
+		var location = '#' + e.target.id;
+		handlePlayerMove(location);
+	});
+
+	function handlePlayerMove(location){
+		$(location).html(playerLetter);
+	}
+
+	function computerMove(){
+
+	}
+
+	$('.game-board').hide();
+})();
