941c89434d083ba34a976d7dd7e52f74a96525ea
Harvey Sanders <sanders.harvey@gmail.com>
2015-11-04 12:47:40 -0600

    fix $.get call. add test button

--- wikipedia_search/js/script.js
+++ wikipedia_search/js/script.js
@@ -1,12 +1,20 @@
 (function(){
-
+	var searchResults = [];
 	function queryWikipedia(query){
 		$.get('http://en.wikipedia.org/w/api.php?action=query&list=search&callback=?&format=json&srsearch=' + encodeURI(query), function(data){
 			console.log(data);
+			searchResults = data.query.search.map(function(result){
+				return result;
+			});
+
 		}, 'json'); //NEED to pass 'json' after callback OR use getJSON(). Look up why later.
 	}
 
 	queryWikipedia('costa rica');
+	
+	$('#test').on('click', function(){
+		console.log(searchResults);
+	});
 
 	
 })();
