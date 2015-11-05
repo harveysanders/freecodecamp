c0edf2559beddd6b1357390825d6993d35a5e5f2
Harvey Sanders <sanders.harvey@gmail.com>
2015-11-04 23:20:22 -0600

    add test result HTML, add search and append function, add initial styling

--- wikipedia_search/js/script.js
+++ wikipedia_search/js/script.js
@@ -1,20 +1,39 @@
 (function(){
 	var searchResults = [];
 	function queryWikipedia(query){
-		$.get('http://en.wikipedia.org/w/api.php?action=query&list=search&callback=?&format=json&srsearch=' + encodeURI(query), function(data){
+		$.get('http://en.wikipedia.org/w/api.php?action=query&list=search&callback=?&format=json&srprop=snippet%7Ctitlesnippet%7Credirecttitle&titles&srsearch=' + encodeURI(query), function(data){
 			console.log(data);
 			searchResults = data.query.search.map(function(result){
-				return result;
+				return createJQueryElement(result);
 			});
+			appendElements(searchResults);
 
 		}, 'json'); //NEED to pass 'json' after callback OR use getJSON(). Look up why later.
 	}
 
-	queryWikipedia('costa rica');
-	
-	$('#test').on('click', function(){
-		console.log(searchResults);
-	});
+	function handleSearchInput(){
+		var query = $('#search').val();
+		queryWikipedia(query);
+	}
+		
+	function createJQueryElement(searchResult){
+		return $('<div>', {
+			'class': 'search-result',
+			html: '<dl><dt>' + searchResult.title + '<dd>' + searchResult.snippet + '...</dd></dt></dl>'
+		});
+	}
 
-	
+	function appendElements(elements){
+		$('.container').append(elements);
+	}
+
+	function spaceToUnderscore(string){
+		return string.replace(' ', '_');
+	}
+
+	$('#search').on('keyup', function(e){
+		if (e.keyCode === 13) {
+			handleSearchInput();
+		}
+	});
 })();
