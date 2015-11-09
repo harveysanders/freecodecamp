f475e4e41343647a8c2250666ce3bbbacec98418
Harvey Sanders <sanders.harvey@gmail.com>
2015-11-09 15:05:32 -0600

    add button lights and sounds, click handlers

--- simon/js/script.js
+++ simon/js/script.js
@@ -1,3 +1,49 @@
 (function(){
 
+	var $greenButton = $('#green-button');
+	var $redButton = $('#red-button');
+	var $yellowButton = $('#yellow-button');
+	var $blueButton = $('#blue-button');
+
+	var greenSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+	var redSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+	var yellowSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+	var blueSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
+
+	function activateButton(buttonColor) {
+		function toggleLight(buttonColor){
+			$('.' + buttonColor).toggleClass(buttonColor + '-lit');
+		}
+
+		switch(buttonColor) {
+			case 'green-button':
+				greenSound.play();
+				toggleLight(buttonColor);
+				setTimeout(function() {toggleLight(buttonColor);}, 500);
+				break;
+
+			case 'red-button':
+				redSound.play();
+				toggleLight(buttonColor);
+				setTimeout(function() {toggleLight(buttonColor);}, 500);
+				break;
+			case 'yellow-button':
+				yellowSound.play();
+				toggleLight(buttonColor);
+				setTimeout(function() {toggleLight(buttonColor);}, 500);
+				break;
+			case 'blue-button':
+				blueSound.play();
+				toggleLight(buttonColor);
+				setTimeout(function() {toggleLight(buttonColor);}, 500);
+				break;
+		}
+	}
+
+	$('.game-button').on('click', function(e){
+		activateButton(e.target.id);
+
+	});
+
+
 })();
