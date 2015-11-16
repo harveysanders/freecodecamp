274e11e12e9dfbccf0a85efc7c1f5f50d49a4672
Harvey Sanders <sanders.harvey@gmail.com>
2015-11-15 18:15:50 -0600

    update blockWin()

--- tic_tac_toe/js/script.js
+++ tic_tac_toe/js/script.js
@@ -25,38 +25,48 @@
 		console.log('remaining spaces: ' + availableSpaces);
 	}
 
-
 	function computerMove(playerLocation){
 		var centerOpen = $('#5').html() === '';
 		if (playerLocation === '5') {
+			console.log('playing corner');
 			playCpuMove(chooseCorner());	
 		} else if (centerOpen) {
+			console.log('playing center');
 			playCpuMove('5');
 		}else if (corners.indexOf(playerLocation ) > -1) {
+			console.log('playing corner 2');
 			playCpuMove(chooseCorner());
 		} else if (edges.indexOf(playerLocation) > -1) {
 			switch(playerLocation) {
 				case '2':
+					console.log('playing opp edge');
 					var spacesNotToPlay = _.union(playerMoves, cpuMoves, ['1','3']);		
 					break;
 				case '4':
+					console.log('playing opp edge');
 					var spacesNotToPlay = _.union(playerMoves, cpuMoves, ['1','7']);		
 					break;
 				case '6':
+					console.log('playing opp edge');
 					var spacesNotToPlay = _.union(playerMoves, cpuMoves, ['3','9']);		
 					break;
 				case '8':
+					console.log('playing opp edge');
 					var spacesNotToPlay = _.union(playerMoves, cpuMoves, ['7','9']);		
 					break;
 			}
 			playCpuMove(getRandomElement(_.difference(corners, spacesNotToPlay)));
 			
+		} else if (_.contains(availableSpaces, blockWin())){
+			console.log('blocking win');
+			playCpuMove(blockWin());
 		} else {
+			console.log('playing any space');
 			playCpuMove(chooseAnyAvailSpace());
 		}
 
 		function chooseCorner() {
-			return getRandomElement(_.difference(corners, availableSpaces));
+			return getRandomElement(_.difference(corners, playerMoves, cpuMoves));
 		}
 
 		function chooseEdge() {
@@ -64,7 +74,17 @@
 		}
 
 		function blockWin(){
-			
+			return getRandomElement(_.flatten(winningSequences.map(function(seqArr){
+				return _.intersection(playerMoves, seqArr);
+			}).filter(function(seqArr){
+				return seqArr.length >= 2; //only show sequences than need one move to win
+			}).map(function(almostWinSeq){ 
+				return winningSequences.map(function(winSeq){
+					return _.difference(winSeq, almostWinSeq); //what are those winning moves?
+				});
+			}), true).filter(function(movesToBlockWin){
+				return movesToBlockWin.length === 1;
+			}))[0];
 		}
 		function chooseAnyAvailSpace() {
 			return getRandomElement(availableSpaces);
