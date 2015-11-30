bfd1ba4054decc5b7647692318a6ac3bfc274395
Harvey Sanders <sanders.harvey@gmail.com>
2015-11-29 20:00:41 -0600

    complete exercise 6

--- learnyounode/ex6-lsFilter.js
+++ learnyounode/ex6-lsFilter.js
@@ -4,6 +4,34 @@ module.exports = function (path, extension, callback) {
 	fs.readdir(path, function(err, files) {
 		if (err)
 			return callback(err);
-		return callback(null, files);
+
+		var filteredFiles = files.filter(function(file) {
+			return file.split('.')[1] === extension;
+		});
+
+		return callback(null, filteredFiles);
 	});
 };
+
+/*
+────────────────────────────────────────────────────────────────────────────────
+solution_filter.js:
+
+    var fs = require('fs')
+    var path = require('path')
+    
+    module.exports = function (dir, filterStr, callback) {
+    
+      fs.readdir(dir, function (err, list) {
+        if (err)
+          return callback(err)
+    
+        list = list.filter(function (file) {
+          return path.extname(file) === '.' + filterStr
+        })
+    
+        callback(null, list)
+      })
+    }
+
+*/
--- learnyounode/ex6-program.js
+++ learnyounode/ex6-program.js
@@ -1,24 +1,30 @@
-var lsFilter = require('./ex6-lsFilter');
+var getFilesWithFilter = require('./ex6-lsFilter');
 
 var path = process.argv[2];
 var extension = process.argv[3];
 var callback = process.argv[4];
 
-lsFilter(path, extension, function (err, files) {
-	var filteredFiles = files.filter(function(file) {
-		return file.split('.')[1] === extension;
-	});
-	filteredFiles.forEach(function(file){
+getFilesWithFilter(path, extension, function (err, files) {
+	files.forEach(function(file){
 		console.log(file);
 	});
 });
 
-// official solution ------------------ //
 /*
-fs.readdir(process.argv[2], function (err, list) {
+────────────────────────────────────────────────────────────────────────────────
+solution.js:
+
+    var filterFn = require('./solution_filter.js')
+    var dir = process.argv[2]
+    var filterStr = process.argv[3]
+    
+    filterFn(dir, filterStr, function (err, list) {
+      if (err)
+        return console.error('There was an error:', err)
+    
       list.forEach(function (file) {
-        if (path.extname(file) === '.' + process.argv[3])
-          console.log(file)
+        console.log(file)
       })
     })
+
 */
