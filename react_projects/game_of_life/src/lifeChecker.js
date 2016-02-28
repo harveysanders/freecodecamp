f15236c1d268b1df005bf869bd581abe7786d620
Harvey Sanders <sanders.harvey@gmail.com>
2016-02-28 01:20:37 -0600

    fix tab/spacing

--- react_projects/game_of_life/src/lifeChecker.js
+++ react_projects/game_of_life/src/lifeChecker.js
@@ -54,51 +54,51 @@
 		return newCells;
 	};
 
-	function countLiveNeighbors(cells, x, y) {
+	const countLiveNeighbors = (cells, x, y) => {
 
 		let count = 0;
 		let gridSize = cells.length;
 		// Check cell on the right.
-	    if (x !== gridSize - 1){
-	        if (cells[y][x + 1].isAlive){ count++; }
-	    }
-	 
-	    // Check cell on the bottom right.
-	    if (x !== gridSize - 1 && y !== gridSize - 1){
-	        if (cells[y + 1][x + 1].isAlive){ count++; }
-	    }
-	 
-	    // Check cell on the bottom.
-	    if (y !== gridSize - 1){
-	        if (cells[y + 1][x].isAlive){ count++; }
-	    }
-	 
-	    // Check cell on the bottom left.
-	    if (x !== 0 && y !== gridSize - 1){
-	        if (cells[y + 1][x - 1].isAlive){ count++; }
-	    }
-	 
-	    // Check cell on the left.
-	    if (x !== 0){
-	        if (cells[y][x - 1].isAlive){ count++; }
-	    }
-	 
-	    // Check cell on the top left.
-	    if (x !== 0 && y !== 0){
-	        if (cells[y - 1][x - 1].isAlive){ count++; }
-	    }
-	 
-	    // Check cell on the top.
-	    if (y !== 0){
-	        if (cells[y - 1][x].isAlive){ count++; }
-	    }
-	 
-	    // Check cell on the top right.
-	    if (x !== gridSize - 1 && y !== 0){
-	        if (cells[y - 1][x + 1].isAlive){ count++; }
-	    }
-	 	
-	    return count;
-	}
+		if (x !== gridSize - 1){
+			if (cells[y][x + 1].isAlive){ count++; }
+		}
+
+		// Check cell on the bottom right.
+		if (x !== gridSize - 1 && y !== gridSize - 1){
+			if (cells[y + 1][x + 1].isAlive){ count++; }
+		}
+
+		// Check cell on the bottom.
+		if (y !== gridSize - 1){
+			if (cells[y + 1][x].isAlive){ count++; }
+		}
+
+		// Check cell on the bottom left.
+		if (x !== 0 && y !== gridSize - 1){
+			if (cells[y + 1][x - 1].isAlive){ count++; }
+		}
+
+		// Check cell on the left.
+		if (x !== 0){
+			if (cells[y][x - 1].isAlive){ count++; }
+		}
+
+		// Check cell on the top left.
+		if (x !== 0 && y !== 0){
+			if (cells[y - 1][x - 1].isAlive){ count++; }
+		}
+
+		// Check cell on the top.
+		if (y !== 0){
+			if (cells[y - 1][x].isAlive){ count++; }
+		}
+
+		// Check cell on the top right.
+		if (x !== gridSize - 1 && y !== 0){
+			if (cells[y - 1][x + 1].isAlive){ count++; }
+		}
+
+		return count;
+	};
 
 })(window);
